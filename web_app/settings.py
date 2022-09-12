e84e6016d1a81d58563a2005cebac508b4bdd1ed
hemachandrika -Full stack developer <75580774+hemachandrika-C@users.noreply.github.com>
2022-09-12 16:17:38 +0530

    Update settings.py

--- web_app/settings.py
+++ web_app/settings.py
@@ -98,7 +98,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER ='testingdjangoapi@gmail.com'
-EMAIL_HOST_PASSWORD = 'gvsxnjabzhiaxztu'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Password validation
